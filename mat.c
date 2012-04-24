134064a0c8ce44eb44411fd91b050d1e43bd505d
Rifat Rahman <rifatrahmanovi@gmail.com>
2012-04-24 15:17:56 +0600

    mat.c modified

--- mat.c
+++ mat.c
@@ -9,40 +9,45 @@
 
 double complex mat[MATSIZE][MATSIZE];
 
-void gen_mat()
+void
+gen_mat ()
 {
-  int i =0,j;
+  int i = 0, j;
   int real;
   int img;
-  for( ;i < MATSIZE; i++)
-  {
-    real = rand()%RAND_RANGE + 1;
-    img = rand()%RAND_RANGE + 1;
-    for(j = MATSIZE; j != i ; j--) 
-    { mat[i][j] = real + img * I;
-      mat[j][i] = conj(mat[i][j]);
+  for (; i < MATSIZE; i++)
+    {
+      real = rand () % RAND_RANGE + 1;
+      img = rand () % RAND_RANGE + 1;
+      for (j = MATSIZE; j != i; j--)
+	{
+	  mat[i][j] = real + img * I;
+	  mat[j][i] = conj (mat[i][j]);
+	}
+      if (i == j)
+	mat[i][i] = rand () % RAND_RANGE + 0 * I;
     }
-    if(i == j)
-      mat[i][i] = rand()%RAND_RANGE + 0*I;
-  }
 }
 
-void print_mat()
+void
+print_mat ()
 {
-  int i,j;
-  for(i = 0; i < MATSIZE; i++)
-  {
-    for(j = 0; j < MATSIZE; j++)
+  int i, j;
+  for (i = 0; i < MATSIZE; i++)
     {
-      printf("%f + %f *i", creal(mat[i][j]), cimag(mat[i][j]));
-      printf("    ");
+      for (j = 0; j < MATSIZE; j++)
+	{
+	  printf ("%f + %f *i", creal (mat[i][j]), cimag (mat[i][j]));
+	  printf ("    ");
+	}
+      puts ("\n");
     }
-    puts("\n");
-  }
 }
-void lapacke_wrapper()
+
+void
+lapacke_wrapper ()
 {
-  int i,j;
+  int i, j;
   char JOBZ = 'V';
   char UPLO = 'U';
   int N = MATSIZE;
@@ -51,25 +56,28 @@ void lapacke_wrapper()
   double complex WORK[2 * MATSIZE];
   double complex A[MATSIZE * MATSIZE];
   int LWORK = 2 * MATSIZE;
-  double RWORK[ 3 * MATSIZE -2];
+  double RWORK[3 * MATSIZE - 2];
   int INFO;
   for (i = 0; i < MATSIZE; i++)
-    for(j = 0; j < MATSIZE; j++)
+    for (j = 0; j < MATSIZE; j++)
       A[(j + MATSIZE * i)] = mat[j][i];
-  zheev_(&JOBZ, &UPLO, &N, A, &LDA, W, WORK, &LWORK, RWORK, &INFO);
+  zheev_ (&JOBZ, &UPLO, &N, A, &LDA, W, WORK, &LWORK, RWORK, &INFO);
   if (!INFO)
-  {
-    printf("OK\n");
-    for(i = 0; i < MATSIZE; i++)
-      printf("%f\n", W[i]);
-  }
-  else printf("Not OK\n");
+    {
+      printf ("OK\n");
+      for (i = 0; i < MATSIZE; i++)
+	printf ("%f\n", W[i]);
+    }
+  else
+    printf ("Not OK\n");
 }
-int main()
+
+int
+main ()
 {
-  srand(time(NULL));
-  gen_mat();
-  print_mat();
-  lapacke_wrapper();
+  srand (time (NULL));
+  gen_mat ();
+  print_mat ();
+  lapacke_wrapper ();
   return 0;
 }
